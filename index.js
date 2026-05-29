dfff4f87c00b4e10c3d97ccf0e7d400d61a1a6e2
Thalyson Alves <thalysonalsilva@outlook.com>
2026-05-29 11:00:32 -0300

    ajustes no javascript e html

--- index.js
+++ index.js
@@ -20,18 +20,12 @@ function newUserTask() {
 
   //selecionando a div new-task-field
   const newTasksField = document.querySelector(".new-tasks-field");
-
-  //criando a div que vai acoplar as novas tarefas e indicando como filho da new-tasks-field
-  const newField = document.createElement('div');
-  newField.appendChild(newTasksField);
-
-  //criando a ul para agrupar as novas tarefas e indicando como filho da div criada anteriormente
-  const newFieldUl = document.createElement('ul');
-  newFieldUl.appendChild(newField);
+  
+  const newTaskGroup = document.querySelector(".new-tasks-group");
 
   //criando o li para mostrar as novas tarefas, indicando como filho do ul e inserindo o value de newTask como conteúdo
   const userTasks = document.createElement('li');
-  userTasks.appendChild(newFieldUl);
+  newTaskGroup.appendChild(userTasks);
   userTasks.textContent = newTask.value;
   
 }
